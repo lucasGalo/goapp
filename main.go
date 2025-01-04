c66e4b62fea19b902e9b5e4422bb0d36e9541b30
sistema <sistema@plataformaoceano.com.br>
2025-01-04 17:38:39 -0300

    other commit

--- main.go
+++ main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main(){
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
-		w.Write([]byte("<h1>Hello world Lucas</h1>"))
+		w.Write([]byte("<h1>Hello world Lucas Galo</h1>"))
 	})
 	http.ListenAndServe(":8181", nil)
 }
